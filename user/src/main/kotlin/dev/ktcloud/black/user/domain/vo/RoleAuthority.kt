cf5aa4e9a2a6b5cc5a9c180a20b0d98224f81a75
K4NEI <kimyoungho0415@gmail.com>
2026-05-13 10:42:27 +0900

    Split modules [common user auth auth-service] to ktcloud-msa-auth-service

--- user/src/main/kotlin/dev/ktcloud/black/user/domain/vo/RoleAuthority.kt
+++
@@ -1,7 +0,0 @@
-package dev.ktcloud.black.user.domain.vo
-
-object RoleAuthority {
-    const val hasUserRole = "hasAnyAuthority('USER')"
-    const val hasAdminRole = "hasAnyAuthority('ADMIN')"
-    const val hasAllRoles = "hasAnyAuthority('USER', 'ADMIN', 'ANONYMOUS')"
-}
